417f92d64ed109e6f4c7649a8b1094b7274b14e5
Anthony Tan <Tony@anthonys-mbp.nyc.rr.com>
2017-12-11 10:32:06 -0500

    Initial Commit

---
+++ MoneyFleek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyFleek
+//
+//  Created by Anthony Tan on 12/11/17.
+//  Copyright © 2017 Anthony Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
